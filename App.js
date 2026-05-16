82fd77b4651460925d5eeb65b4ecb1dfba007544
Aparna Suman <aparnasuman2@gmail.com>
2026-05-16 11:54:46 +0530

    react js with core javascript

---
+++ App.js
@@ -0,0 +1,45 @@
+const heading = React.createElement(
+  "div",
+  { id: "parent", xyz: "abc" },
+
+  React.createElement(
+    "div",
+    { id: "child" },
+
+    [
+      React.createElement(
+        "h1",
+        { key: 1 },
+        "I am a big fan"
+      ),
+
+      React.createElement(
+        "h1",
+        { key: 2 },
+        "I am a big fan1"
+      ),
+
+      React.createElement(
+        "h1",
+        { key: 3 },
+        "I am a big fan2"
+      )
+    ]
+  )
+);
+
+React.createElement("div",{id:"child1"},[
+       React.createElement(
+        "h1",
+        { key: 1 },
+        "I am a big fan"
+      ),
+])
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+
+console.log(heading);
+
+root.render(heading);
